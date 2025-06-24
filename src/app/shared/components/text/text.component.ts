4fa5ad6f6a52afccd2ce049879af03355be328f2
RafaelKC <rafaelkaua97@gmail.com>
2025-06-24 19:58:33 -0300

    FIN-21 added more options to fin-text

--- src/app/shared/components/text/text.component.ts
+++ src/app/shared/components/text/text.component.ts
@@ -1,18 +1,26 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 export type TextType = 'label' | 'title' | 'subtitle' | 'paragraph' | 'info';
+export type LinkTarget = '_self' | '_blank' | '_parent' | '_top';
 
 @Component({
   selector: 'fin-text',
   templateUrl: './text.component.html',
   styleUrl: './text.component.scss',
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, RouterLink],
 })
 export class TextComponent {
   public readonly type = input<TextType>('paragraph');
   public readonly textColor = input('inherit');
   public readonly backgroundColor = input('transparent');
   public readonly text = input('');
+
+  // Link Props
+  public readonly href = input<string>();
+  public readonly target = input<LinkTarget>('_self');
+  public readonly routerLink = input<string | any[]>();
+  public readonly rel = input<string>();
 }
